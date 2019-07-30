00c4bb8b7036eefe011d81172fea423d713cdf12
Małgorzata Stolarczyk <gosia.stolarczyk@gmail.com>
2019-07-30 20:28:00 +0200

    fix images

--- src/js/script.js
+++ src/js/script.js
@@ -137,53 +137,62 @@
     processOrder(){
       const thisProduct = this;
       console.log('processOrder', thisProduct);
+      const imagesSelectors = [];
 
-      /* read all data from the form (using utils.serializeFormToObject) and save it to const formData */
-      const formData = utils.serializeFormToObject(thisProduct.form);
-      console.log('formData', formData);
-      /* set variable price to equal thisProduct.data.price */
-      let price = thisProduct.data.price;
-      /* START LOOP: for each paramId in thisProduct.data.params */
-      for(let paramId in thisProduct.data.params){
-        /* save the element in thisProduct.data.params with key paramId as const param */
-        const param = thisProduct.data.params[paramId];
-        /* START LOOP: for each optionId in param.options */
-        for(let optionId in param.options){
-          /* save the element in param.options with key optionId as const option */
-          const option = param.options[optionId];
+       /* read all data from the form (using utils.serializeFormToObject) and save it to const formData */
+       const formData = utils.serializeFormToObject(thisProduct.form);
+       console.log('formData', formData);
+       /* set variable price to equal thisProduct.data.price */
+       let price = thisProduct.data.price;
+       /* START LOOP: for each paramId in thisProduct.data.params */
+       for(let paramId in thisProduct.data.params){
+         /* save the element in thisProduct.data.params with key paramId as const param */
+         const param = thisProduct.data.params[paramId];
+         /* START LOOP: for each optionId in param.options */
+         for(let optionId in param.options){
+           /* save the element in param.options with key optionId as const option */
+           const option = param.options[optionId];
 
-          const optionSelected = formData.hasOwnProperty(paramId) && formData[paramId].indexOf(optionId) > -1;
-          /* START IF: if option is selected and option is not default */
-          if(optionSelected && !option.default){
-            /* add price of option to variable price */
-            price += option.price;
-          /* END IF: if option is selected and option is not default */
-          } else if(!optionSelected && option.default) {
-          /* START ELSE IF: if option is not selected and option is default */
-            price -= option.price;
-            /* deduct price of option from price */
-          }
+           const optionSelected = formData.hasOwnProperty(paramId) && formData[paramId].indexOf(optionId) > -1;
+           /* START IF: if option is selected and option is not default */
 
+           // IMAGES
+           if(optionSelected) imagesSelectors.push(`.${paramId}-${optionId}`);
 
-          const productImages = thisProduct.data.images;
-          const optionSelectedImage = ...
-          console.log('image', productImages);
-          for(let productImage of productImages){
-              if(productImage == optionSelectedImage){
-              productImage.classList.add(classNames.menuProduct.imageVisible);
-              } else {
-              productImage.classList.remove(classNames.menuProduct.imageVisible);
-              }
-          }
-          /* END ELSE IF: if option is not selected and option is default */
-        }
-        /* END LOOP: for each optionId in param.options */
-      }
-      /* END LOOP: for each paramId in thisProduct.data.params */
-      /* set the contents of thisProduct.priceElem to be the value of variable price */
-      thisProduct.priceElem.innerHTML = price;
-    }
-  }
+           if(optionSelected && !option.default){
+             /* add price of option to variable price */
+             price += option.price;
+           /* END IF: if option is selected and option is not default */
+           } else if(!optionSelected && option.default) {
+           /* START ELSE IF: if option is not selected and option is default */
+             price -= option.price;
+             /* deduct price of option from price */
+           }
+
+           // IMAGES
+
+           const imageWrapper = thisProduct.imageWrapper[0];
+           for(let image of imageWrapper.querySelectorAll('img')) {
+             image.classList.remove('active');
+           }
+
+           imageWrapper.querySelectorAll('img')[0].classList.add('active');
+
+           for(let selector of imagesSelectors) {
+             const imageElem = imageWrapper.querySelector(selector);
+             if(imageElem) imageElem.classList.add('active');
+             console.log(imageElem);
+           }
+
+           /* END ELSE IF: if option is not selected and option is default */
+         }
+         /* END LOOP: for each optionId in param.options */
+       }
+       /* END LOOP: for each paramId in thisProduct.data.params */
+       /* set the contents of thisProduct.priceElem to be the value of variable price */
+       thisProduct.priceElem.innerHTML = price;
+     }
+   }
 
   const app = {
     initMenu: function(){
